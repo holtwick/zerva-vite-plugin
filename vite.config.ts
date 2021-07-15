43a9242a5e268474ee38da03efc11076e58e3097
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-07-15 22:07:23 +0200

    Use socket browser and arrange more nicely

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import { viteZervaPlugin } from "./src/module"
+import { viteZervaPlugin } from "./plugin"
 import { useSocketIO } from "zerva-socketio"
 
 useSocketIO({})
