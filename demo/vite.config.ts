ab22338e44322654d9755aa98a83675a13890767
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-08-08 22:29:11 +0200

    Complete the demo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import { viteZervaPlugin } from "./plugin"
+import { viteZervaPlugin } from "zerva-vite-plugin"
 import { useSocketIO } from "zerva-socketio"
 import { on } from "zerva"
 
@@ -12,11 +12,6 @@ const log = Logger("demo")
 on("httpInit", ({ get }) => {
   get("/zerva", `Hello, this is Zerva!`)
   get("/data.json", ({ req, res }) => {
-    // log("req", Object.keys(req))
-    // log("res", Object.keys(res))
-    // res.setHeader("Cache-Control", "no-cache")
-    // res.setHeader("Content-Type", "text/json")
-    // res.setHeader("Access-Control-Allow-Origin", "*")
     return { hello: "world" }
   })
 })
