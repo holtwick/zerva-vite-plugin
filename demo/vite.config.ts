3f5b6f6ca0b87bad8819c299b50db1e724d2e36c
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-08-30 21:31:43 +0200

    Extend the demo showing custom socket events

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import { viteZervaPlugin } from "zerva-vite-plugin"
-import { useSocketIO } from "zerva-socketio"
-import { on } from "zerva"
-
+import { defineConfig } from "vite"
 import { Logger } from "zeed"
+import { on } from "zerva"
+import { useSocketIO } from "zerva-socketio"
+import { viteZervaPlugin } from "zerva-vite-plugin"
+import "./src/protocol"
+
 const log = Logger("demo")
 
 const zervaSetup = async () => {
   useSocketIO()
+
+  on("socketIOConnect", (conn) => {
+    conn.on("viteEcho", (data: any) => data)
+  })
+
   on("httpInit", ({ get, addStatic }) => {
     get("/zerva", `Hello, this is Zerva!`)
     get("/data.json", ({ req, res }) => {
